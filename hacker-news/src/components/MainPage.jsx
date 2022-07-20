aeab00370d671a4b6168e14c9cc5ecc0b2419134
Vladik <vlad.a.aleynikov@gmail.com>
2022-07-20 16:30:45 +0300

    feat: add news on main page

--- hacker-news/src/components/MainPage.jsx
+++ hacker-news/src/components/MainPage.jsx
@@ -3,18 +3,26 @@ import Loading from './UI/Loading';
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchNews } from "../store/newsReducer";
+import Post from './Post';
 
 
 const MainPage = () => {
       const news = useSelector(state => state.newsReducer.news);
       const dispatch = useDispatch();
+      const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
       useEffect(() => {
             dispatch(fetchNews());
       }, [])
+
       return (
             (!news.length) ?
                   <Loading /> :
-                  <div>main</div>
+                  news.map(post =>
+                        <Post title={post.title}
+                              author={post.by}
+                              score={post.score}
+                              date={new Date(post.time).toLocaleDateString("en-US")}
+                              id={post.id} />)
       )
 }
 
---
+++ hacker-news/src/components/Post.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Post = ({ title, author, score, date, id, ...props}) => {
+
+  return (
+    <NavLink to={"/"+id}>
+      <h3>{title}</h3>
+      <div className="rating">Story score: {score}</div>
+      <div className="author">By {author}</div>
+      <div className="creation-date">Creation date: {date}</div>
+    </NavLink>
+  )
+}
+
+export default Post
