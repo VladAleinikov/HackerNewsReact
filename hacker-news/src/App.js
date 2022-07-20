66611dd4026d3c6a17fa4d275e462a466085efeb
Vladik <vlad.a.aleynikov@gmail.com>
2022-07-20 15:28:15 +0300

    feat: create loading component

--- hacker-news/src/App.js
+++ hacker-news/src/App.js
@@ -1,6 +1,4 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchNews } from "./store/newsReducer";
+
 import { BrowserRouter, Route } from 'react-router-dom';
 import AppRouter from "./components/AppRouter";
 
@@ -8,13 +6,9 @@ import AppRouter from "./components/AppRouter";
 
 function App() {
 
-  const news = useSelector(state => state.newsReducer.news);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchNews());
-  }, [])
 
-  console.log(news);
+
+
   return (
     <BrowserRouter>
       <AppRouter/>
--- hacker-news/src/components/MainPage.jsx
+++ hacker-news/src/components/MainPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Loading from './UI/Loading';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchNews } from "../store/newsReducer";
+
 
 const MainPage = () => {
-  return (
-    <div>MainPage</div>
-  )
+      const news = useSelector(state => state.newsReducer.news);
+      const dispatch = useDispatch();
+      useEffect(() => {
+            dispatch(fetchNews());
+      }, [])
+      return (
+            (!news.length) ?
+                  <Loading /> :
+                  <div>main</div>
+      )
 }
 
 export default MainPage
