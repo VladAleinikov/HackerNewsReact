992b7e4b2fc82b765d59e6e6e71c2c8035cb5d8e
Vladik <vlad.a.aleynikov@gmail.com>
2022-07-25 16:36:25 +0300

    fix: fix long fetching

--- hacker-news/src/redux/saga/newsSaga.js
+++ hacker-news/src/redux/saga/newsSaga.js
@@ -2,18 +2,17 @@ import { put, takeEvery, call } from "redux-saga/effects"
 import { FETCH_NEWS, setNews } from "../store/newsReducer";
 
 
-const fetchMaxNewsId = () => fetch('https://hacker-news.firebaseio.com/v0/maxitem.json?print=pretty');
+const fetchNewestNews = () => fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
 
 function* fetchNewsWorker() {
-      const maxNewsIdPromise = yield call(fetchMaxNewsId);
-      const maxNewsId = yield call(() => new Promise(res => res(maxNewsIdPromise.json())))
+      const newestNewsPromise = yield call(fetchNewestNews);
+      const newestNews = yield call(() => new Promise(res => res(newestNewsPromise.json())))
       let json = [];
-      for (let i = maxNewsId; json.length < 10; i--) {
+      for (let i = 0; json.length < 10; i--) {
             console.log('a');
-            const news = yield call(() => fetch(`https://hacker-news.firebaseio.com/v0/item/${i}.json?print=pretty`));
+            const news = yield call(() => fetch(`https://hacker-news.firebaseio.com/v0/item/${newestNews[i]}.json?print=pretty`));
             json.push(yield call(() => new Promise(res => res(news.json()))));
-            if (json[json.length - 1].type !== 'story')
-                  json.pop();
+
       }
       console.log(json);
       yield put(setNews(json));
